1e4d3c730ebde389380478dd6a1eab1bf3651ff9
Dalia <Dalia@DESKTOP-5QIQF3U>
2018-01-21 18:32:37 +0200

    first commit

---
+++ src/firstproject/test.java
@@ -0,0 +1,12 @@
+package firstproject;
+
+public class test {
+public static void main(String args[]){
+	int x=5;
+	int y=7;
+	int d=x+y;
+	System.out.println("Hello world!");
+	System.out.print("result="+d);
+	
+}
+}
